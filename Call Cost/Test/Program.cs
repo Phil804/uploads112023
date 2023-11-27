06596ab379dc86266b1c747cacf13eb9515c02c1
Phillip Jackson <pj37883@email.vccs.edu>
2023-11-27 18:30:35 -0500

    ChatAWhile push description

---
+++ Call Cost/Test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ChatAWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Calculated variables to output cost of call based on area code and duration of call
+
+            int[] areaCode = { 262, 414, 608, 715, 815, 920 };
+            double[] rate = { 0.07, 0.10, 0.05, 0.16, 0.24, 0.14 };
+
+
+
+            Write("Please enter your area code: ");
+            int inputArea = Convert.ToInt32(ReadLine());
+
+            Write("How many minutes will this call be? ");
+            double callMins = Convert.ToDouble(ReadLine());
+
+
+            for (int i = 0; i < areaCode.Length; i++)
+            {
+                if (inputArea == areaCode[i])
+                {
+                    double total = callMins * rate[i];
+                    WriteLine("The cost of this call will be " + total.ToString("C") + ".");
+                };
+
+            }
+
+            ReadKey();
+
+
+        }
+    }
+}
---
+++ Chat-A-While 2/Chat-A-While 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ChatAWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Program take in user's entry and calculates rates
+            
+            int[] areaCode = { 262, 414, 608, 715, 815, 920 };
+            double[] rate = { 0.07, 0.10, 0.05, 0.16, 0.24, 0.14 };
+
+
+
+            Write("Please enter your area code: ");
+            int inputArea = Convert.ToInt32(ReadLine());
+
+            Write("How many minutes will this call be? ");
+            double callMins = Convert.ToDouble(ReadLine());
+
+
+            for (int i = 0; i < areaCode.Length; i++)
+            {
+                if (inputArea == areaCode[i])
+                {
+                    double total = callMins * rate[i];
+                    WriteLine("The cost of this call will be " + total.ToString("C") + ".");
+                };
+
+            }
+
+            ReadKey();
+
+
+        }
+    }
+}
